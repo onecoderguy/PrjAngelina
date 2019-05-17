6c72086ac730075c5ddb00e1f2abc77a23a991f3
Luís Felipe <maisumrasta@gmail.com>
2019-05-17 11:29:36 -0300

    Criação da classe escola

---
+++ ProjetoLP/AngelinaPrj/Models/Escola.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AngelinaPrj.Models
+{
+    [Table("Escolas")]
+    public class Escola
+    {
+        public int EscolaId { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Nome { get; set; }
+
+        [Required]
+        [MaxLength(70)]
+        public string Cidade { get; set; }
+    }
+}
